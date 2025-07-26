29f19d2c4c92c1d8ae442db6acc8ac210b02ccf6
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2025-07-26 18:34:18 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (60.26%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,40 @@
+class Solution {
+    int[] dx = {0, 1}; // right and down
+    int[] dy = {1, 0};
+
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        // Initialize dp with -1 (unvisited)
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        return helper(dp, 0, 0, m, n);
+    }
+
+    public int helper(int[][] dp, int row, int col, int m, int n) {
+        if (row == m - 1 && col == n - 1) {
+            return 1;
+        }
+
+        if (row >= m || col >= n) return 0;
+
+        if (dp[row][col] != -1) return dp[row][col];
+
+        int count = 0;
+
+        for (int i = 0; i < 2; i++) {
+            int rx = row + dx[i];
+            int cy = col + dy[i];
+
+            if (rx < m && cy < n) {
+                count += helper(dp, rx, cy, m, n);
+            }
+        }
+
+        return dp[row][col] = count;
+    }
+}
