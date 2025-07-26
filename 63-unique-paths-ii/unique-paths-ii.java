5006c889a5317e374a1f19d720e7d612e5701faa
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2025-07-26 19:04:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (96.68%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -0,0 +1,44 @@
+class Solution {
+    int[] dx = {0, 1}; // right and down
+    int[] dy = {1, 0};
+    int[][] obstacleGrid; // make it accessible to helper
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        this.obstacleGrid = obstacleGrid;
+
+        // If starting cell has obstacle, return 0
+        if (obstacleGrid[0][0] == 1) return 0;
+
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        return helper(dp, 0, 0, m, n);
+    }
+
+    public int helper(int[][] dp, int row, int col, int m, int n) {
+        // If out of bounds or hitting an obstacle
+        if (row >= m || col >= n || obstacleGrid[row][col] == 1) return 0;
+
+        // Reached bottom-right
+        if (row == m - 1 && col == n - 1) return 1;
+
+        if (dp[row][col] != -1) return dp[row][col];
+
+        int count = 0;
+
+        for (int i = 0; i < 2; i++) {
+            int rx = row + dx[i];
+            int cy = col + dy[i];
+
+            count += helper(dp, rx, cy, m, n);
+        }
+
+        return dp[row][col] = count;
+    }
+}
