04c22625fa54b4aefc1de77e86298a5d64d7c193
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2025-07-30 10:15:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.java
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.java
@@ -1,29 +1,19 @@
-// User function Template for Java
-
 class Solution {
     static int knapSack(int val[], int wt[], int capacity) {
-        // code here
         int n = val.length;
-        // int w  = capacity;
-        int dp[][]= new int [n+1][capacity+1];
-        for(int i =0;i<=n;i++){
-            dp[i][0] = 0;
-        }
-        for(int j=0;j<=capacity;j++){
-            dp[0][j] = 0;
-        }
-        
-        for(int i =1;i<=n;i++){
-            for(int j =1;j<=capacity;j++){
-                int v= val[i-1];
-                int w = wt[i-1];
-                if(w<=j){
-                    dp[i][j] = Math.max(dp[i-1][j],v+dp[i][j-w]);
-                }else{
-                    dp[i][j] = dp[i-1][j];
+        int dp[][] = new int[n + 1][capacity + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= capacity; j++) {
+                int notTake = dp[i - 1][j];
+                int take = Integer.MIN_VALUE;
+                if (wt[i - 1] <= j) {
+                    take = dp[i][j - wt[i - 1]] + val[i - 1]; // reuse same item
                 }
+                dp[i][j] = Math.max(notTake, take);
             }
         }
+
         return dp[n][capacity];
     }
 }
