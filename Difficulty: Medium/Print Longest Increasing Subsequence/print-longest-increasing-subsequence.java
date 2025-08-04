b54e8257b72485b06f4c79bfc1cbac590136779f
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2025-08-04 08:51:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Print Longest Increasing Subsequence/print-longest-increasing-subsequence.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> getLIS(int arr[]) {
+        int n = arr.length;
+        
+        int[] dp = new int[n];      // dp[i] = length of LIS ending at i
+        int[] hash = new int[n];    // To track predecessors
+        Arrays.fill(dp, 1);
+
+        for (int i = 0; i < n; i++) {
+            hash[i] = i;
+        }
+
+        int maxLen = 1;
+        int lastIndex = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int prev = 0; prev < i; prev++) {
+                if (arr[i] > arr[prev] && dp[i] < dp[prev] + 1) {
+                    dp[i] = dp[prev] + 1;
+                    hash[i] = prev;
+                }
+            }
+
+            // Update the lastIndex of the LIS found so far
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                lastIndex = i;
+            }
+        }
+
+        // Reconstruct LIS using hash[]
+        ArrayList<Integer> lis = new ArrayList<>();
+        lis.add(arr[lastIndex]);
+        while (hash[lastIndex] != lastIndex) {
+            lastIndex = hash[lastIndex];
+            lis.add(arr[lastIndex]);
+        }
+
+        // Since we built it from the back, reverse it
+        Collections.reverse(lis);
+        return lis;
+    }
+}
