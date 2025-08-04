3edbbdfe1abbd516a2a853512a8c2f43b254061f
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2025-08-04 09:07:52 +0530

    Time: 5 ms (92.87%) | Memory: 44.3 MB (66.59%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -22,20 +22,42 @@ import java.util.*;
 //     }
 // }
 
+// class Solution {
+//       public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+//         int dp[]= new int [n];
+//         Arrays.fill(dp,1);
+//         int max = Integer.MIN_VALUE;
+//         for(int i =0;i<n;i++){
+//             for(int j=0;j<i;j++){
+//                 if(nums[j]<nums[i]){
+//                     dp[i] =Math.max(dp[j]+1,dp[i]);
+//                 }
+//                 max = Math.max(max,dp[i]);
+//             }
+//         }
+//         return max==Integer.MIN_VALUE?1:max;
+//       }
+// }
+
 class Solution {
       public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int dp[]= new int [n];
-        Arrays.fill(dp,1);
-        int max = Integer.MIN_VALUE;
-        for(int i =0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    dp[i] =Math.max(dp[j]+1,dp[i]);
-                }
-                max = Math.max(max,dp[i]);
+            List<Integer> tails = new ArrayList<>();
+
+        for (int num : nums) {
+            int index = Collections.binarySearch(tails, num);
+
+            if (index < 0) {
+                index = -index - 1; // Insertion point
+            }
+
+            if (index == tails.size()) {
+                tails.add(num); // Extend the sequence
+            } else {
+                tails.set(index, num); // Replace to maintain minimal tail
             }
         }
-        return max==Integer.MIN_VALUE?1:max;
+
+        return tails.size();
       }
 }
