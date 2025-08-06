78f738f7883ee2ca783e5c038f72110914f93e97
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2025-08-06 07:18:40 +0530

    Time: 7 ms (54.18%) | Memory: 42.4 MB (29.94%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -1,26 +1,42 @@
+// class Solution {
+//     public int maxSumAfterPartitioning(int[] arr, int k) {
+//         int n = arr.length;
+//         int[] dp = new int[n];
+//         Arrays.fill(dp, -1);
+//         return f(0, arr, k, dp);
+//     }
+//     public int f(int i, int[] arr, int k, int[] dp) {
+//         int n = arr.length;
+//         if (i == n) return 0;
+//         if (dp[i] != -1) return dp[i];
+//         int maxInPartition = Integer.MIN_VALUE;
+//         int maxSum = Integer.MIN_VALUE;
+
+//         for (int j = i; j < Math.min(n, i + k); j++) {
+//             maxInPartition = Math.max(maxInPartition, arr[j]);
+//             int length = j - i + 1;
+//             int sum = maxInPartition * length + f(j + 1, arr, k, dp);
+//             maxSum = Math.max(maxSum, sum);
+//         }
+
+//         return dp[i] = maxSum;
+//     }
+// }
 class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
-        int n = arr.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        return f(0, arr, k, dp);
-    }
-
-    public int f(int i, int[] arr, int k, int[] dp) {
-        int n = arr.length;
-        if (i == n) return 0;
-        if (dp[i] != -1) return dp[i];
-
+    int n =arr.length;
+    int dp[]= new int[n+1];
+    for(int i =n-1;i>=0;i--){
         int maxInPartition = Integer.MIN_VALUE;
         int maxSum = Integer.MIN_VALUE;
-
-        for (int j = i; j < Math.min(n, i + k); j++) {
-            maxInPartition = Math.max(maxInPartition, arr[j]);
+         for (int j = i; j < Math.min(n, i + k); j++) {
+             maxInPartition = Math.max(maxInPartition, arr[j]);
             int length = j - i + 1;
-            int sum = maxInPartition * length + f(j + 1, arr, k, dp);
+            int sum = maxInPartition * length + dp[j+1];
             maxSum = Math.max(maxSum, sum);
-        }
-
-        return dp[i] = maxSum;
+         }
+         dp[i] = maxSum;
+    }
+    return dp[0];
     }
 }
