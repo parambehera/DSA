9db739bec688ef70879c1d00a56137ed8549b6db
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2025-08-07 09:22:52 +0530

    Time: 24 ms (25.43%) | Memory: 44.9 MB (65.65%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,38 +1,72 @@
+// class Solution {
+//     public boolean isMatch(String s, String p) {
+//         int n = s.length();
+//         int m = p.length();
+//         int[][] dp = new int[n][m]; // -1 = uncomputed, 0 = false, 1 = true
+
+//         for (int i = 0; i < n; i++)
+//             for (int j = 0; j < m; j++)
+//                 dp[i][j] = -1;
+
+//         return f(n - 1, m - 1, dp, s, p);
+//     }
+
+//     boolean f(int i, int j, int[][] dp, String s, String p) {
+//         if (i < 0 && j < 0) return true;
+//         if (j < 0 && i >= 0) return false;
+//         if (i < 0 && j >= 0) {
+//             for (int k = 0; k <= j; k++) {
+//                 if (p.charAt(k) != '*') return false;
+//             }
+//             return true;
+//         }
+
+//         if (dp[i][j] != -1) return dp[i][j] == 1;
+
+//         boolean ans;
+//         if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?') {
+//             ans = f(i - 1, j - 1, dp, s, p);
+//         } else if (p.charAt(j) == '*') {
+//             ans = f(i - 1, j, dp, s, p) || f(i, j - 1, dp, s, p);
+//         } else {
+//             ans = false;
+//         }
+
+//         dp[i][j] = ans ? 1 : 0;
+//         return ans;
+//     }
+// }
+
 class Solution {
     public boolean isMatch(String s, String p) {
         int n = s.length();
         int m = p.length();
-        int[][] dp = new int[n][m]; // -1 = uncomputed, 0 = false, 1 = true
 
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++)
-                dp[i][j] = -1;
+        boolean[][] dp = new boolean[n + 1][m + 1];
+        dp[0][0] = true;
 
-        return f(n - 1, m - 1, dp, s, p);
-    }
-
-    boolean f(int i, int j, int[][] dp, String s, String p) {
-        if (i < 0 && j < 0) return true;
-        if (j < 0 && i >= 0) return false;
-        if (i < 0 && j >= 0) {
-            for (int k = 0; k <= j; k++) {
-                if (p.charAt(k) != '*') return false;
+        // Initialize first row for empty string s
+        for (int j = 1; j <= m; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 1];
+            } else {
+                dp[0][j] = false;
             }
-            return true;
         }
 
-        if (dp[i][j] != -1) return dp[i][j] == 1;
-
-        boolean ans;
-        if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?') {
-            ans = f(i - 1, j - 1, dp, s, p);
-        } else if (p.charAt(j) == '*') {
-            ans = f(i - 1, j, dp, s, p) || f(i, j - 1, dp, s, p);
-        } else {
-            ans = false;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
         }
 
-        dp[i][j] = ans ? 1 : 0;
-        return ans;
+        return dp[n][m];
     }
 }
+
