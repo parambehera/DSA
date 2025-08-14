2917915b653b40599128f791da2b49b79f2e73c6
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2025-08-14 06:41:24 +0530

    Time: 9 ms (16.18%) | Memory: 42.8 MB (23.66%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
@@ -1,36 +1,56 @@
-import java.util.HashMap;
+// import java.util.HashMap;
+
+// class Solution {
+//     public String largestGoodInteger(String num) {
+//         HashMap<Character, Integer> map = new HashMap<>();
+//         int left = 0;
+//         int right = 0;
+//         String ans = "";
+
+//         while (right < num.length()) {
+//             char ch = num.charAt(right);
+//             map.put(ch, map.getOrDefault(ch, 0) + 1);
+//             // Shrink window if more than 1 unique char
+//             while (map.size() > 1) {
+//                 char lch = num.charAt(left);
+//                 map.put(lch, map.get(lch) - 1);
+//                 if (map.get(lch) == 0) {
+//                     map.remove(lch);
+//                 }
+//                 left++;
+//             }
+
+//             // Check for 3 consecutive same digits
+//             if (map.size() == 1 && map.get(ch) == 3) {
+//                 String curr = num.substring(left, right + 1);
+//                 if (ans.isEmpty() || curr.compareTo(ans) > 0) {
+//                     ans = curr;
+//                 }
+//             }
+//             right++;
+//         }
+
+//         return ans;
+//     }
+// }.
+
 
 class Solution {
     public String largestGoodInteger(String num) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        int left = 0;
-        int right = 0;
         String ans = "";
-
-        while (right < num.length()) {
-            char ch = num.charAt(right);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-
-            // Shrink window if more than 1 unique char
-            while (map.size() > 1) {
-                char lch = num.charAt(left);
-                map.put(lch, map.get(lch) - 1);
-                if (map.get(lch) == 0) {
-                    map.remove(lch);
-                }
-                left++;
-            }
-
-            // Check for 3 consecutive same digits
-            if (map.size() == 1 && map.get(ch) == 3) {
-                String curr = num.substring(left, right + 1);
+        for (int i = 0; i <= num.length() - 3; i++) {
+            char c1 = num.charAt(i);
+            char c2 = num.charAt(i + 1);
+            char c3 = num.charAt(i + 2);
+            if (c1 == c2 && c2 == c3) {
+                String curr = "" + c1 + c2 + c3;
                 if (ans.isEmpty() || curr.compareTo(ans) > 0) {
                     ans = curr;
                 }
             }
-            right++;
         }
-
         return ans;
     }
 }
+
+
