a9ab3392eb0ae13219389e2ae70d95a5d47ad8b5
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2025-08-20 06:22:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in fully rotated sorted 2D matrix/search-in-fully-rotated-sorted-2d-matrix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean searchMatrix(int[][] mat, int x) {
+        // code here
+        ArrayList <Integer> list = new ArrayList <>();
+        for(int i=0;i<mat.length;i++){
+            for(int j =0;j<mat[0].length;j++){
+              if(mat[i][j]==x)return true;
+            }
+        }
+        return false;
+       
+        
+        
+    }
+  
+}
