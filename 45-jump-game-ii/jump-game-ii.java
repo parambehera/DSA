b2539323f8a9af1c1633dffd1c66161719ddeffe
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2025-08-30 10:44:12 +0530

    Time: 1 ms (99.54%) | Memory: 45.6 MB (24.59%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int jump(int[] nums) {
+          int jumps = 0;
+        int currEnd = 0;
+        int currFarthest = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            currFarthest = Math.max(currFarthest, i + nums[i]);
+
+            if (i == currEnd) {  // time to make a jump
+                jumps++;
+                currEnd = currFarthest;
+            }
+        }
+
+        return jumps;
+    }
+}
