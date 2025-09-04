898f4cc5a8506b64abacb32c88e5850f84c70170
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2025-09-04 15:52:17 +0530

    Time: 3 ms (99.68%) | Memory: 45.3 MB (68.58%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,51 +1,38 @@
 class Solution {
-    public boolean canFinish(int n, int[][] prerequisites) {
-              ArrayList<Integer>[] graph = new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            graph[i] = new ArrayList<>();
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+            ArrayList<Integer>[] adj = new ArrayList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            adj[i] = new ArrayList<>();
         }
 
+        // Build adjacency list
         for (int i = 0; i < prerequisites.length; i++) {
-            int dest =prerequisites[i][0];
             int src = prerequisites[i][1];
-            graph[src].add(dest);
+            int dest = prerequisites[i][0];
+            adj[src].add(dest);
         }
 
-        return kahnTopoSort(graph,n).size()==n;
+        boolean[] vis = new boolean[numCourses];
+        boolean[] stack = new boolean[numCourses]; // recursion stack
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!vis[i]) {
+                if (isCyclic(adj, i, vis,stack)) return false; // cycle found
+            }
+        }
+        return true; // no cycle
     }
-      public static ArrayList<Integer> kahnTopoSort(ArrayList<Integer>[] graph, int V) {
-        int[] indegree = new int[V];
-        ArrayList<Integer> result = new ArrayList<>();
-
-      
-        for (int u = 0; u < V; u++) {
-            for (int v : graph[u]) {
-                indegree[v]++;
+    public boolean isCyclic(ArrayList <Integer> adj[],int curr,boolean vis[],boolean stack []){
+        stack[curr] = true;
+        vis[curr] = true;
+        for(int i =0;i<adj[curr].size();i++){
+            int neigh = adj[curr].get(i);
+            if(stack[neigh]) return true;
+            if(!vis[neigh] && isCyclic(adj,neigh,vis,stack)){
+                return true;
             }
         }
-
-     
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < V; i++) {
-            if (indegree[i] == 0) {
-                q.add(i);
-            }
-        }
-
-        
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            result.add(node);
-
-            for (int neighbor : graph[node]) {
-                indegree[neighbor]--;
-                if (indegree[neighbor] == 0) {
-                    q.add(neighbor);
-                }
-            }
-        }
-
-      
-        return result;
+        stack [curr] = false;
+        return false;
     }
 }
