c86834c953564a2b34e931c0282a1d0b2efc05c0
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2025-10-02 12:20:41 +0530

    Time: 2 ms (99.73%) | Memory: 50.3 MB (55.82%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,26 +1,38 @@
+import java.util.*;
+
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Arrays.sort(nums);
         int n = nums.length;
-        int req = (int) Math.floor(n / 3);
-        int cnt = 1;
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (!list.isEmpty()) {
-                if (cnt > req && list.get(list.size()-1) != nums[i]) {
-                    list.add(nums[i]);
-                }
-            }else{
-                if(cnt>req){
-                    list.add(nums[i]);
-                }
-            }
-            if (i < n - 1 && nums[i] == nums[i + 1]) {
-                cnt++;
-            } else if (i < n - 1 && nums[i] != nums[i + 1]) {
-                cnt = 1;
+        int el1 = 0, el2 = 1; // initialize differently to avoid confusion
+        int cnt1 = 0, cnt2 = 0;
+
+        // 1st pass: find potential candidates
+        for (int num : nums) {
+            if (num == el1) cnt1++;
+            else if (num == el2) cnt2++;
+            else if (cnt1 == 0) {
+                el1 = num;
+                cnt1 = 1;
+            } else if (cnt2 == 0) {
+                el2 = num;
+                cnt2 = 1;
+            } else {
+                cnt1--;
+                cnt2--;
             }
         }
-        return list;
+
+        // 2nd pass: check actual counts
+        cnt1 = 0;
+        cnt2 = 0;
+        for (int num : nums) {
+            if (num == el1) cnt1++;
+            else if (num == el2) cnt2++;
+        }
+
+        List<Integer> res = new ArrayList<>();
+        if (cnt1 > n / 3) res.add(el1);
+        if (cnt2 > n / 3) res.add(el2);
+        return res;
     }
 }
