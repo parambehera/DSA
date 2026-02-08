bfb9a18f862e7ca072715a8b544d152b2effc860
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2026-02-08 19:28:59 +0530

    Time: 43 ms (34.91%) | Memory: 47.8 MB (45.84%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -17,28 +17,51 @@
 //     }
 // }
 
+// class Solution {
+//     public int minimumDeletions(String s){
+//       int n = s.length();
+//       int lb[]= new int [n];
+//       int cntlb=0;
+//       int ra[]= new int [n];
+//       int cntra=0;
+//       for(int i=0;i<n;i++){
+//           lb[i]=cntlb;
+//           if(s.charAt(i)=='b'){
+//             cntlb++;
+//           }
+//       }
+//       for(int i=n-1;i>=0;i--){
+//         ra[i]=cntra;
+//         if(s.charAt(i)=='a'){
+//             cntra++;
+//         }
+//       }
+//       int min =Integer.MAX_VALUE;
+//       for(int i=0;i<n;i++){
+//         min = Math.min(lb[i]+ra[i],min);
+//       }
+//       return min;
+//     }
+// }
 class Solution {
     public int minimumDeletions(String s){
       int n = s.length();
-      int lb[]= new int [n];
       int cntlb=0;
-      int ra[]= new int [n];
       int cntra=0;
       for(int i=0;i<n;i++){
-          lb[i]=cntlb;
-          if(s.charAt(i)=='b'){
-            cntlb++;
-          }
-      }
-      for(int i=n-1;i>=0;i--){
-        ra[i]=cntra;
         if(s.charAt(i)=='a'){
             cntra++;
         }
       }
-      int min =Integer.MAX_VALUE;
+       int min =Integer.MAX_VALUE;
       for(int i=0;i<n;i++){
-        min = Math.min(lb[i]+ra[i],min);
+          if(s.charAt(i)=='a'){
+            cntra--;
+          }
+           min = Math.min(cntlb+cntra,min);
+          if(s.charAt(i)=='b'){
+            cntlb++;
+          }
       }
       return min;
     }
