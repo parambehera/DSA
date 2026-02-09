13901c607a0a1d843c53f6d536114b0bb054ce36
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2026-02-09 18:06:58 +0530

    Time: 1530 ms (21.14%) | Memory: 48 MB (99.31%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,25 +1,37 @@
 class Solution {
-    public int subarraySum(int[] nums, int k) {
-        int n = nums.length;
-      int prefixSum[]= new int [n];
+    // public int subarraySum(int[] nums, int k) {
+    //     int n = nums.length;
+    //   int prefixSum[]= new int [n];
+    //   int cnt =0;
+    //   prefixSum[0] = nums[0];
+    //   for(int i=1;i<n;i++){
+    //     prefixSum[i] =prefixSum[i-1]+nums[i]; 
+    //   }
+    //   for(int i=0;i<n;i++){
+    //     for(int j=i;j<n;j++){
+    //         int sum=0;
+    //         if(i!=0){
+    //                 sum =prefixSum[j]-prefixSum[i-1];
+    //         }else{
+    //                sum =prefixSum[j]; 
+    //         }
+    //         if(sum==k){
+    //            cnt++;
+    //         }          
+    //     }
+    //   }
+    //   return cnt;
+    // }
+     public int subarraySum(int[] nums, int k) {
+      int n = nums.length;
       int cnt =0;
-      prefixSum[0] = nums[0];
-      for(int i=1;i<n;i++){
-        prefixSum[i] =prefixSum[i-1]+nums[i]; 
-      }
       for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
-            int sum=0;
-            if(i!=0){
-                    sum =prefixSum[j]-prefixSum[i-1];
-            }else{
-                   sum =prefixSum[j]; 
-            }
-            if(sum==k){
-               cnt++;
-            }          
+        int sum=0;
+        for(int j =i;j<n;j++){
+          sum+=nums[j];
+          if(sum==k)cnt++;
         }
-      }
+      } 
       return cnt;
     }
 }
