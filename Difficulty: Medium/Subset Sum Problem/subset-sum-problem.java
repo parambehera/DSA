af507a12619e68bff5a598853de981358677bd76
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2026-03-16 10:18:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -1,34 +1,37 @@
 class Solution {
 
-    static Boolean[][] memo;
-
     static Boolean isSubsetSum(int arr[], int sum) {
-        int n = arr.length;
-        memo = new Boolean[n][sum + 1];
-
-        return helper(arr, n-1, sum);
+        // code here
+        int n =arr.length;
+        boolean dp[][]= new boolean [n+1][sum+1];
+        
+        for(int i=0;i<=n;i++){
+            dp[i][0] = true;
+        }
+        
+        for(int i = n-1;i>=0;i--){
+            for(int j = 1;j<=sum;j++){
+                boolean take = false;
+                if(j>=arr[i]){
+                    take = dp[i+1][j-arr[i]];
+                }
+                boolean notTake = dp[i+1][j];
+                dp[i][j] = take || notTake;
+            }
+        }
+        return dp[0][sum];
+        
     }
-
-    static boolean helper(int arr[], int i, int target) {
-
-        // If target becomes 0 → subset found
-        if (target == 0) return true;
-
-        // If reached end or target negative
-        if (i == -1 || target < 0) return false;
-
-        // If already computed
-        if (memo[i][target] != null)
-            return memo[i][target];
-
-        // Take element
-        boolean take = helper(arr, i - 1, target - arr[i]);
-
-        // Not take element
-        boolean notTake = helper(arr, i - 1, target);
-
-        memo[i][target] = take || notTake;
-
-        return memo[i][target];
+    static Boolean Sum(int arr[],int idx,int sum){
+        if(sum==0){
+            return true;
+        }
+        if(idx==arr.length){
+            return false;
+        }
+        
+        boolean take = Sum(arr,idx+1,sum-arr[idx]);
+        boolean notTake = Sum(arr,idx+1,sum);
+        return take || notTake;
     }
 }
