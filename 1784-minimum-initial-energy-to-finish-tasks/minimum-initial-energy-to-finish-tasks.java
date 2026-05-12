26a2bc75fa04804e0e7a99c78df9bce8d3ee3612
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2026-05-12 11:10:56 +0530

    Time: 31 ms (21.15%) | Memory: 114 MB (40.38%) - LeetSync

--- 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.java
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.java
@@ -1,12 +1,64 @@
+// class Solution {
+//     public int minimumEffort(int[][] tasks) {
+
+//         Arrays.sort(tasks, (a, b) -> {
+//             return (b[1] - b[0]) - (a[1] - a[0]);
+//         });
+
+//         int energy = 0;
+//         int current = 0;
+
+//         for (int[] task : tasks) {
+
+//             int actual = task[0];
+//             int minimum = task[1];
+
+//             if (current < minimum) {
+//                 energy += (minimum - current);
+//                 current = minimum;
+//             }
+
+//             current -= actual;
+//         }
+
+//         return energy;
+//     }
+// }
 class Solution {
+
     public int minimumEffort(int[][] tasks) {
 
-        Arrays.sort(tasks, (a, b) -> {
-            return (b[1] - b[0]) - (a[1] - a[0]);
-        });
+        Arrays.sort(tasks, (a, b) ->
+            (b[1] - b[0]) - (a[1] - a[0])
+        );
 
-        int energy = 0;
-        int current = 0;
+        int low = 1;
+        int high = 0;
+
+        for (int[] task : tasks) {
+            high += task[1];
+        }
+
+        int ans = high;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (canFinish(tasks, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public boolean canFinish(int[][] tasks, int energy) {
+
+        int current = energy;
 
         for (int[] task : tasks) {
 
@@ -14,13 +66,12 @@ class Solution {
             int minimum = task[1];
 
             if (current < minimum) {
-                energy += (minimum - current);
-                current = minimum;
+                return false;
             }
 
             current -= actual;
         }
 
-        return energy;
+        return true;
     }
 }
