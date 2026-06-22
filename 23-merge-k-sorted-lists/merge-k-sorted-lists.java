fe917c12a45f69ba0be62282d1845263110b5dda
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2026-06-22 12:14:00 +0530

    Time: 4 ms (82.10%) | Memory: 46.7 MB (63.36%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,45 +1,32 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
+
     public ListNode mergeKLists(ListNode[] lists) {
-    int n  = lists.length;
-    ListNode temp = new ListNode(-1);
-    if(n==0) return null;
-    if(n==1)return lists[0];
-    ListNode h = merge(lists[0],lists[1]);
-    for(int i =2;i<lists.length;i++){
-       h = merge(lists[i],h);
-    }
-    return h;
-    }
-    public ListNode merge(ListNode l1,ListNode l2 ){
-      ListNode dummy = new ListNode(-1);
-        ListNode temp = dummy;
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                temp.next = l1;
-                l1 = l1.next;
-            } else {
-                temp.next = l2;
-                l2 = l2.next;
+
+        PriorityQueue<ListNode> pq =
+                new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        // Insert the head of every list
+        for (ListNode node : lists) {
+            if (node != null)
+                pq.offer(node);
+        }
+
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+
+        while (!pq.isEmpty()) {
+
+            ListNode node = pq.poll();
+
+            tail.next = node;
+            tail = tail.next;
+
+            // Insert next node from the same list
+            if (node.next != null) {
+                pq.offer(node.next);
             }
-            temp = temp.next;
         }
-        if (l1 != null) {
-            temp.next = l1;
-        } else if (l2 != null) {
-            temp.next = l2;
-        }
-        
+
         return dummy.next;
-     
     }
 }
