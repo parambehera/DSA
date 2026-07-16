5ab2642acfd1d1fc128f0786e7b212d8ad4f3368
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2026-07-16 11:33:10 +0530

    Time: 443 ms (5.03%) | Memory: 171.7 MB (5.09%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,26 +1,33 @@
-class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        ArrayList <Integer> list = new ArrayList <>();
-       Deque <Integer> dq = new LinkedList<>();
+class Solution { 
+    public int[] maxSlidingWindow(int[] nums, int k) { 
+        TreeMap<Integer, Integer> map = new TreeMap<>(); 
+        int n = nums.length; 
+        int max = Integer.MIN_VALUE; 
+        ArrayList<Integer> list = new ArrayList<>(); 
 
-       for(int i =0;i<nums.length;i++){
-        if(!dq.isEmpty() && dq.peekFirst()<=i-k){
-            dq.pollFirst();
-        }
-        while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i]){
-            dq.pollLast();
-        }
-         dq.offerLast(i);
-         
-         if(i >= k-1){
-          list.add(nums[dq.peekFirst()]); // ✅ correct
+        for(int i = 0; i < k; i++) { 
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            max = Math.max(nums[i], max); 
+        } 
+        list.add(max); 
 
-         }
-       }
-        int res[]= new int [list.size()];
-        for(int i =0;i<res.length;i++){
-            res[i] = list.get(i);
-        }
-        return res;
-    }
+        for(int right = k; right < n; right++) { 
+            int last = right - k; 
+            int ele = nums[last]; 
+            if (map.get(ele) == 1) {
+                map.remove(ele);
+            } else {
+                map.put(ele, map.get(ele) - 1);
+            }
+            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+            int m = map.lastKey(); 
+            list.add(m); 
+        } 
+
+        int arr[] = new int[list.size()]; 
+        for(int i = 0; i < list.size(); i++) { 
+            arr[i] = list.get(i); 
+        } 
+        return arr; 
+    } 
 }
