5da1afed59cb9a6f1779d8631de81cf99b867221
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2026-07-25 20:07:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.java
+++ Difficulty: Medium/Frog Jump/frog-jump.java
@@ -1,17 +1,26 @@
 class Solution {
     int minCost(int[] height) {
+        // code here
         int n = height.length;
-        int[] dp = new int[n];
-        
-        dp[0] = 0;
-        for (int i = 1; i < n; i++) {
-            int oneStep = dp[i - 1] + Math.abs(height[i] - height[i - 1]);
-            int twoStep = Integer.MAX_VALUE;
-            if (i > 1) {
-                twoStep = dp[i - 2] + Math.abs(height[i] - height[i - 2]);
-            }
-            dp[i] = Math.min(oneStep, twoStep);
+        int dp[]=  new int [n];
+        for(int i=0;i<n;i++){
+            dp[i] = -1;
         }
-        return dp[n - 1];
+        helper(height,n-1,dp);
+        return dp[n-1]==-1?0:dp[n-1];
+    }
+    public int helper(int hight[],int idx,int dp[]){
+        if(idx==0){
+            return 0;
+        }
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+        int cost1 = helper(hight,idx-1,dp)+Math.abs(hight[idx]-hight[idx-1]);
+        int cost2 = Integer.MAX_VALUE;
+        if(idx>1){
+             cost2 = helper(hight,idx-2,dp)+Math.abs(hight[idx]-hight[idx-2]);
+        }
+        return dp[idx] = Math.min(cost1,cost2);
     }
 }
