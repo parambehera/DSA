16e8efcaa68aea7467aa8ed525eac84e22912df6
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2026-07-26 19:15:37 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (5.45%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,18 +1,29 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length==1){
-            return nums[0];
-        }
-        if(nums.length==2){
-            return Math.max(nums[0],nums[1]);
-        }
-        int n = nums.length;
-        int dp[]= new int [n];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
-        for(int  i=2;i<n;i++){
-            dp[i] = Math.max(nums[i]+dp[i-2],dp[i-1]);
-        }
-        return dp[n-1];
+        int dp[][] = new int[nums.length][2];
+
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+
+        return helper(nums, false, dp, 0);
+    }
+
+    public int helper(int nums[], boolean prev, int dp[][], int idx) {
+        if (idx == nums.length)
+            return 0;
+
+        int p = prev ? 1 : 0;
+
+        if (dp[idx][p] != -1)
+            return dp[idx][p];
+
+        int rob = Integer.MIN_VALUE;
+
+        if (!prev)
+            rob = nums[idx] + helper(nums, true, dp, idx + 1);
+
+        int skip = helper(nums, false, dp, idx + 1);
+
+        return dp[idx][p] = Math.max(rob, skip);
     }
 }
