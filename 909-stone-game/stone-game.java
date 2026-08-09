7f0bb2ae9a4ff4c3d928e46f24d2829c213e7e19
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2026-08-09 17:42:53 +0530

    Time: 9 ms (17.75%) | Memory: 46.6 MB (28.59%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,5 +1,26 @@
 class Solution {
+    int[][] memo;
+
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+        memo = new int[n][n];
+
+        for (int[] row : memo)
+            Arrays.fill(row, -1);
+
+        return dp(piles, 0, n - 1) > 0;
+    }
+
+    int dp(int[] piles, int i, int j) {
+        if (i == j)
+            return piles[i];
+
+        if (memo[i][j] != -1)
+            return memo[i][j];
+
+        int left = piles[i] - dp(piles, i + 1, j);
+        int right = piles[j] - dp(piles, i, j - 1);
+
+        return memo[i][j] = Math.max(left, right);
     }
 }
