a4d6076485a5e709210b81379b842666e2028287
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2026-08-10 08:46:49 +0530

    Time: 7 ms (30.03%) | Memory: 46 MB (30.78%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,26 +1,29 @@
 class Solution {
-    int[][] memo;
-
     public boolean stoneGame(int[] piles) {
-        int n = piles.length;
-        memo = new int[n][n];
+        int n =piles.length;
+        int dp[][]= new int [n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=piles[i];
+        }
+        int alice = helper(piles,dp,0,n-1);
+        return alice > (sum-alice);
 
-        for (int[] row : memo)
-            Arrays.fill(row, -1);
-
-        return dp(piles, 0, n - 1) > 0;
     }
+    public int helper(int piles[],int dp[][],int i,int j ){
+        if(i>j){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
 
-    int dp(int[] piles, int i, int j) {
-        if (i == j)
-            return piles[i];
+        int takei = piles[i]+Math.min(helper(piles,dp,i+2,j),helper(piles,dp,i+1,j-1));
+        int takej = piles[j]+Math.min(helper(piles,dp,i,j-2),helper(piles,dp,i+1,j-1));
 
-        if (memo[i][j] != -1)
-            return memo[i][j];
-
-        int left = piles[i] - dp(piles, i + 1, j);
-        int right = piles[j] - dp(piles, i, j - 1);
-
-        return memo[i][j] = Math.max(left, right);
+        return dp[i][j] = Math.max(takei,takej);
     }
 }
