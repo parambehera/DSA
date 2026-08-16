40c84f62d85c43a5ba471fd1801637b4512e135a
Param Jyoti Behera <paramjyotibehera2005@gmail.com>
2026-08-16 11:56:27 +0530

    Time: 539 ms (10.90%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,5 +1,3 @@
-# Write your MySQL query statement below
-select email
-from Person 
-group by email
-having count(email)>1;
+# Write your MySQL query statement belows
+-- select count(email) as countEmail ,email from person group by email;
+select email as "Email" from person  group by email having count(email) >1;
